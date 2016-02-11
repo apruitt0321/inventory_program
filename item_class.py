d91efdfef170027cf225f3ceafdccf035b937733
Andrew Pruitt <apruitt0321@users.noreply.github.com>
2016-02-10 17:17:01 -0700

    Created item class file

---
+++ item_class.py
@@ -0,0 +1,34 @@
+#Defines the class Item
+class Item(object):
+    
+    def __init__(self, name, count):
+        self.name = name
+        self.count = count
+        self.loc = "No location has been set yet"
+    
+    def __str__(self):
+        return " Item Name: " + self.name + "\n Current Count: " + str(self.count) + "\n Current Location: " + self.loc
+    
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def add_one(self):
+        self.count = self.count + 1
+    
+    def set_total(self, total):
+        self.count = total
+    
+    def remove_one(self):
+        self.count = self.count - 1
+    
+    def change_loc(self, new_loc):
+        self.loc = new_loc
+    
+    def get_name(self):
+    	return self.name
+    
+    def get_count(self):
+    	return self.count
+    
+    def get_loc(self):
+    	return self.loc
